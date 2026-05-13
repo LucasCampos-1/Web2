f44268288e564e76afd8fcafcfb2ed044af6e1d5
Lucas Campos <lucasguilherme0787@gmail.com>
2026-05-13 11:43:25 -0300

    Atividades_01

---
+++ Atividade_01/database/migrations/2026_05_13_143410_create_books_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('author_id')->unsigned();
+            $table->integer('publisher_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
