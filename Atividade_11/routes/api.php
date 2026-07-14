48396b987d0324611b6ac373050209b3ee7de128
Lucas Campos <lucasguilherme0787@gmail.com>
2026-07-14 02:35:52 -0300

    feat: route

--- Atividade_11/routes/api.php
+++ Atividade_11/routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('books', BookApiController::class);
+Route::apiResource('books', BookApiController::class)->names('api.books');
